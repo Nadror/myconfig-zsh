8ca43868a0ab887c0425b47ad218878b1665b1c3
Nadror <renauxmaximilienpro@gmail.com>
2023-01-08 02:31:46 +0100

    Com + add install.sh

--- config/nadror.zsh-theme
+++ config/nadror.zsh-theme
@@ -3,7 +3,7 @@
 if [[ "$TERM" != "dumb" ]] && [[ "$DISABLE_LS_COLORS" != "true" ]]; then
     PROMPT='[%{$fg[red]%}%n%{$reset_color%}@%{$fg[magenta]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}$(git_prompt_info)]# '
 
-    ZSH_THEME_GIT_PROMPT_PREFIX=" - %{$fg[green]%}"
+    ZSH_THEME_GIT_PROMPT_PREFIX=" - %{$fg[green]%}" #Show current branch (master for example)
     ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
     ZSH_THEME_GIT_PROMPT_DIRTY=""
     ZSH_THEME_GIT_PROMPT_CLEAN=""
---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+###Install zsh git wget####
+echo -e "Check if we have zsh, git and wget ...\n"
+    if [ -d /usr/bin/wget ] && [ -d /usr/bin/git ] && [ -d /usr/bin/zsh ]; then
+        echo -e "zsh wget and git are already installed\n"
+    else
+        echo -e "You need to install at your own : 'sudo apt install -y zsh git wget'"
+    fi
+
+###Install oh-my-zsh####
+echo -e "Installing oh-my-zsh...\n"
+    if [ -d ~/.oh-my-zsh ]; then
+        echo -e "oh-my-zsh is already installed, please remove --> /home/$USER/.oh-my-zsh" && exit
+    else
+        
+        cd /tmp
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    fi
+
+###Install autosuggestions plugin###
+echo -e "Installing autosuggestions plugin...\n"
+    if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+        echo -e "autosuggestions plugin is already installed\n"
+    else
+        mkdir -p ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    fi
+
+###Install my config in recent USER
+echo -e "Install my config ZSH...\n"
+    if [ -d ~/.zshrc ]; then
+        mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d")# Backup .zshrc
+        echo -e "File already existed .zshrc to .zshrc-'date' backup created.\n"
+    elif [ -d /tmp/myconfig-zsh ]; then
+        cd /tmp/myconfig-zsh
+        mkdir -p ~/.oh-my-zsh/custom/themes/
+        cp -f /tmp/myconfig-zsh/config/nadror.zsh-theme ~/.oh-my-zsh/custom/themes/nadror.zsh-theme
+        cp -f /tmp/myconfig-zsh/config/.zshrc ~/.zshrc
+        source ~/.zshrc # Refresh the profile zsh shell
+        echo -e "Config installed\n"
+        zsh
+    else
+        git clone --depth 1 https://github.com/Nadror/myconfig-zsh.git
+        cd /tmp/myconfig-zsh
+        mkdir -p ~/.oh-my-zsh/custom/themes/
+        cp -f /tmp/myconfig-zsh/config/nadror.zsh-theme ~/.oh-my-zsh/custom/themes/
+        cp -f /tmp/myconfig-zsh/config/.zshrc ~/
+        source ~/.zshrc
+        echo -e "Config installed\n"
+        zsh
+    fi
+exit
+
+#Credit
+#ohmyzsh: https://ohmyz.sh/
+#zsh: https://www.zsh.org/
+#plugin : https://github.com/zsh-users/zsh-autosuggestions
+#Pierre i saw you
